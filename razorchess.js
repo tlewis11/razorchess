cb489312af01b0ec84dbbc00c0e4866d08e75bb2
tlewis11 <tlewis@kenzan.com>
2016-05-21 22:27:07 -0600

    calculate returns the move after stockfish calculates

--- razorchess.js
+++ razorchess.js
@@ -20,25 +20,65 @@ app.get('/game', function(req, res){
     res.sendfile('./public/index.html');
 });
 
-function run_cmd(cmd, args, cb, end) {
+//call bash cmd - use this for communicating with stockfish
+/*function run_cmd(cmd, args, cb, end) {
     var spawn = require('child_process').spawn,
         child = spawn(cmd, args),
         me = this;
     child.stdout.on('data', function (buffer) { cb(me, buffer) });
     child.stdout.on('end', end);
-}
+}*/
 
 
 app.get('/calculate', function(req, res){
 
-    var foo = new run_cmd(
-        'netstat', ['-an'],
-        function (me, buffer) { me.stdout += buffer.toString() },
-        function () { res.send(me.stdout) }
-    );
+
+
+
+    var cmd = '/kano/razorchess/stockfish/stockfish-7-mac/Mac/stockfish-7-64'
+    var spawn = require('child_process').spawn;
+
+    //to decode the buffer
+    var StringDecoder = require('string_decoder').StringDecoder;
+    var decoder = new StringDecoder('utf8');
+
+    stockfish = spawn(cmd);
+
+    //need to grab this stodout and make some decision on how to make the move
+    stockfish.stdout.on('data', (data) => {
+        var message = decoder.write(data);
+        console.log(message.trim());
+
+        var search_test = message.trim().search('bestmove'); 
+        console.log(search_test);
+        if (search_test != -1) {
+            
+            console.log('looks good ------ ' + message.trim());
+            res.send(message.trim().split(" ")[5]);  
+        }
+    });
+
+    console.log('======== send readyok ========');
+    stockfish.stdin.write('isready\n');
+    
+
+    console.log('======== send fen string ========');
+
+    stockfish.stdin.write('position fen r1bqkbnr/pp1ppppp/2n5/2p5/3PP3/5N2/PPP2PPP/RNBQKB1R b KQkq - 0 3\n');
+    
+    stockfish.stdin.write('go movetime 1000\n');
+    console.log('======== get the move ========');
+    console.log('======== respond with  move ========');
+
+    fen_string = '' 
+    //res.send(fen_string)
 
 });
 
 
 
 app.listen(8081)
+//=======
+//runs stockfish executable and outputs uci responses to console
+
+
