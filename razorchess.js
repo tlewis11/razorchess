a887c307699e79a2c30d4ded4c8d562e5b2f9e62
bpeno <blakepeno1991@gmail.com>
2016-07-15 15:01:00 -0500

    trivial change to add morgan (request logger). also compiled the chessboard, apparently.

--- razorchess.js
+++ razorchess.js
@@ -5,13 +5,13 @@ var engine = stockfish();
 
 //gonna set this here since the engine is kind enough to wait for us to do it
 engine.onmessage = (event) => {
-    var data = (event.data ? event.data : event);
-    var index = data.search('bestmove');
-    //only return best move...
-    if (index != -1) {
-        //second part is the actual move
-        engine.res.send(data.split(' ')[index + 1]);
-    }
+	var data = (event.data ? event.data : event);
+	var index = data.search('bestmove');
+	//only return best move...
+	if (index != -1) {
+		//second part is the actual move
+		engine.res.send(data.split(' ')[index + 1]);
+	}
 };
 
 
@@ -19,29 +19,26 @@ var app = express();
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-var myLogger = function (req, res, next) {
-    next();
-};
+// morgan: https://github.com/expressjs/morgan
+app.use(morgan('common'));
 
-app.use(myLogger);
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.get('/game', function (req, res) {
-    res.sendfile('./public/index.html');
+app.get('/game', function(req, res) {
+	res.sendFile(__dirname + '/public/index.html');
 });
 
-
 //takes fen, returns 'best' move
-app.get('/calculate', function (req, res) {
+app.get('/calculate', function(req, res) {
 
-    var fen_string = req.query.fen;
+	var fen_string = req.query.fen;
 
-    engine.res = res;
-    console.log(engine.onmessage.toString());
-    engine.postMessage('isready');
-    engine.postMessage('position fen ' + fen_string);
-    engine.postMessage('go movetime 1000');
+	engine.res = res;
+	console.log(engine.onmessage.toString());
+	engine.postMessage('isready');
+	engine.postMessage('position fen ' + fen_string);
+	engine.postMessage('go movetime 1000');
 });
 
 
